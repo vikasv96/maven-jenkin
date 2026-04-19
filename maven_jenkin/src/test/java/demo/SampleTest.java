dc173a36fe72a85ba73b52262902971c8604bbbb
vikas <vikas@Vikas>
2026-04-19 14:43:18 +0530

    First commit

---
+++ maven_jenkin/src/test/java/demo/SampleTest.java
@@ -0,0 +1,19 @@
+package demo;
+
+import org.testng.annotations.Test;
+
+public class SampleTest {
+
+	@Test
+
+	public void case1() {
+		System.out.println("Test will be execute....");
+
+	}
+	
+	public void case11() {
+		System.out.println("Test will be execute....");
+
+	}
+
+}
---
+++ maven_jenkin/src/test/java/sample/DemoTest.java
@@ -0,0 +1,18 @@
+package sample;
+
+import org.testng.annotations.Test;
+
+public class DemoTest {
+	@Test
+
+	public void case1() {
+		System.out.println("Test will be execute....");
+
+	}
+	
+	public void case11() {
+		System.out.println("Test will be execute....");
+
+	}
+
+}
