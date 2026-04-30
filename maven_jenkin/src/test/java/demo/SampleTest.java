31670bf538b9ef58d355595bc115b9e9698f4b06
vikasv96 <vikasngbu@gmail.com>
2026-04-30 23:47:57 +0530

    Update SampleTest.java

--- maven_jenkin/src/test/java/demo/SampleTest.java
+++ maven_jenkin/src/test/java/demo/SampleTest.java
@@ -13,13 +13,10 @@ public class SampleTest {
 	
 	public void case11() {
 		System.out.println("case11");
-<<<<<<< HEAD
 
 	}
 	public void case111() {
 		System.out.println("case111");
-=======
->>>>>>> branch 'master' of https://github.com/vikasv96/maven-jenkin.git
 
 	}
 
