7e8c1c4e82d3f6b2f13bd6f084eec14259351147
vikas <vikas@Vikas>
2026-04-30 23:19:04 +0530

    casetest

---
+++ maven_jenkin/src/test/java/test/CaseTest.java
@@ -0,0 +1,16 @@
+package test;
+
+import org.testng.annotations.Test;
+
+public class CaseTest {
+
+	@Test
+
+	public void Test1() {
+		System.out.println("Test1");
+	}
+
+	public void Test2() {
+		System.out.println("Test2");
+	}
+}
